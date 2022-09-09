53f75aa3e760d9ee1a049a49c9a7c6d6d84697f5
karan sivarat <bomb0069@gmail.com>
2022-09-09 12:34:53 +0700

    [Added]: CreditApprovalService

--- src/main/java/com/siamchamnankit/demo/controller/CrediteController.java
+++ src/main/java/com/siamchamnankit/demo/controller/CrediteController.java
@@ -1,12 +1,21 @@
 package com.siamchamnankit.demo.controller;
 
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.siamchamnankit.demo.service.CreditApprovalService;
+
 @RestController
 public class CrediteController {
-    @PostMapping("/api/v1/credit/request")
-    public boolean creditRequest() {
-        return true;
+    @Autowired
+    CreditApprovalService creditApprovalService;
+
+    @PostMapping("/api/v1/credit/request/{requestAmount}")
+    public boolean creditRequest(@PathVariable BigDecimal requestAmount) {
+        return creditApprovalService.request(requestAmount);
     }
 }
---
+++ src/main/java/com/siamchamnankit/demo/service/CreditApprovalService.java
@@ -0,0 +1,14 @@
+package com.siamchamnankit.demo.service;
+
+import java.io.Console;
+import java.math.BigDecimal;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreditApprovalService {
+    public boolean request(BigDecimal requestAmount) {
+        return true;
+    }
+
+}
